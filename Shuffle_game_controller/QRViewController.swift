ff3867c4e7f967f249c7f6e2cc7c214a1b79fd5b
John Johnston <johnj@proteas-macbook-pro.local>
2016-03-21 13:03:04 -0400

    Fixed Questions to display correct and 2 random incorrect answers. Repaired modal segue to prevent multiple popovers. Added code to go back to new plant.

--- Shuffle_game_controller/QRViewController.swift
+++ Shuffle_game_controller/QRViewController.swift
@@ -43,7 +43,7 @@ class QRViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate
         // MOD
         plants += ["cotton", "oats", "mint", "chamomile", "beet", "willow", "echinacea", "alfalfa", "madder", "calendula", "wheat", "flax", "lemon_balm", "eyeball_plant", "onion", "bedstraw", "gypsywort", "pumpkin", "sweet_pea", "st_johns_wort", "sunflower", "amaranth", "lavender", "aloe", "poppy", "buckwheat", "ramie", "red_clover", "fullers_teasel", "ginger"] // case sensitive name of plants images in assess folder
         answers += ["the jeans I wear", "the cereal, granola, or cookies I eat", "flavoring in my toothpaste", "the tea I drink to help me feel better", "food coloring", "medicine I take for a headache", "medicine I take for a cold", "food for cattle", "dying fabrics red", "dying fabrics yellow", "the bread I eat", "making linen sheets", "tea I drink and candy I eat", "treating toothaches", "practicing how to use a microscope", "the bed I sleep in", "dying fabrics black", "making a holiday pie", "making perfume", "treating depression", "making cooking oil", "dying fabrics red and as an edible grain", "making essential oils, perfumes and cosmetics", "treating a sunburng", "making medicine to treat pain", "making medicine for diabetes and for stuffing pillows", "making packing materials and fishing nets", "making medicine to lower cholesterol", "cleaning fabircs used in making clothes" ,"making medicine to treat an upset stomach"]
-
+        randomPlant = array
     }
     
     // SEGUE MOD
--- Shuffle_game_controller/QuestionsViewController.swift
+++ Shuffle_game_controller/QuestionsViewController.swift
@@ -12,7 +12,7 @@ import GameplayKit
 class QuestionsViewController: UIViewController {
     
     // MOD
-    var array = 0
+    var array = Int()
     
     var plants = [String]()
     //var color = 0
@@ -21,23 +21,45 @@ class QuestionsViewController: UIViewController {
     var numQuestions = 0 // this the how many questions the player is asked
     var score = 0 //this is how many questions the player has answered correctly
     var donePlants = [Int:String]() // this keeps track of which plants have already been shown
-    
+    var ranIndex1 = Int()
+    var ranIndex2 = Int()
     
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
-
+    
+    // MOD FOR DEVEL
+    @IBOutlet weak var rightanswer: UILabel!
+    @IBOutlet weak var ranAnswer1: UILabel!
+    @IBOutlet weak var ranAnswer2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         plants += ["cotton", "oats", "mint", "chamomile", "beet", "willow", "echinacea", "alfalfa", "madder", "calendula", "wheat", "flax", "lemon_balm", "eyeball_plant", "onion", "bedstraw", "gypsywort", "pumpkin", "sweet_pea", "st_johns_wort", "sunflower", "amaranth", "lavender", "aloe", "poppy", "buckwheat", "ramie", "red_clover", "fullers_teasel", "ginger"]
         answers += ["the jeans I wear", "the cereal, granola, or cookies I eat", "flavoring in my toothpaste", "the tea I drink to help me feel better", "food coloring", "medicine I take for a headache", "medicine I take for a cold", "food for cattle", "dying fabrics red", "dying fabrics yellow", "the bread I eat", "making linen sheets", "tea I drink and candy I eat", "treating toothaches", "practicing how to use a microscope", "the bed I sleep in", "dying fabrics black", "making a holiday pie", "making perfume", "treating depression", "making cooking oil", "dying fabrics red and as an edible grain", "making essential oils, perfumes and cosmetics", "treating a sunburng", "making medicine to treat pain", "making medicine for diabetes and for stuffing pillows", "making packing materials and fishing nets", "making medicine to lower cholesterol", "cleaning fabircs used in making clothes" ,"making medicine to treat an upset stomach"]
+        
         askQuestion()
     }
     
     func askQuestion(action: UIAlertAction! = nil) {
         
+        // MOD
+        rightanswer.text = answers[array]   // This is the answer corresponding to the selected plant
+        ranIndex1 = GKRandomSource.sharedRandom().nextIntWithUpperBound(answers.count)          // Generate the first random incorrect answer
+        while ranIndex1 == array {
+            ranIndex1 = GKRandomSource.sharedRandom().nextIntWithUpperBound(answers.count)      // This makes sure I don't repeat a question choice
+        }
+        ranIndex2 = GKRandomSource.sharedRandom().nextIntWithUpperBound(answers.count)            // Generate the second random incorrect answer
+        while (ranIndex1 == ranIndex2) && (ranIndex2 != array) {
+                ranIndex2 = GKRandomSource.sharedRandom().nextIntWithUpperBound(answers.count)    // This makes sure I don't repeat a question choice
+            }
+        ranAnswer1.text = answers[ranIndex1]
+        ranAnswer2.text = answers[ranIndex2]
+        return
     }
     
+    
+
     func gotoEnding(action: UIAlertAction! = nil) {
         self.performSegueWithIdentifier("segueToScoreViewController", sender: nil)  //This function can be called to start the segue to the score page
     }
